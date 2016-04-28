fd520cba6d81bd4cbe12188da56b3a487b55920f
Sam Berg <SBerg@VHB.com>
2016-04-28 17:56:46 -0400

    feature layer reporter

---
+++ js/featureLayerReporter.js
@@ -0,0 +1,221 @@
+﻿
+define([
+  'dojo/_base/declare',
+  "dojo/Deferred",
+  "dojo/parser",
+  "dojo/dom",
+  "esri/map", 
+  "esri/urlUtils",
+  "esri/arcgis/utils",
+  "dojo/_base/array",
+  "esri/request",
+  "esri/config",
+  "dojo/request/xhr",
+  "esri/lang",
+  "dojo/json",
+  "dojo/dom", "esri/tasks/query", "esri/tasks/QueryTask", "esri/tasks/RelationshipQuery", "esri/layers/FeatureLayer", "dojo/dom-construct"
+], function(
+  declare,
+  Deferred,
+  parser,
+  dom,
+  Map,
+  urlUtils,
+  arcgisUtils,
+  array,
+  esriRequest,
+  esriConfig,
+  xhr,esriLang,json,dom,
+  Query, QueryTask, RelationshipQuery, FeatureLayer,domConstruct
+) {
+
+
+    return declare(null,{
+     
+        generateLayerReport: function (layer) {
+            esriConfig.defaults.io.proxyUrl = "/proxy/proxy.ashx";
+            var deferred = new Deferred();
+
+            var layerReport = {};
+
+            var queryTask = new QueryTask(layer.url);
+            var sourceFeatureLayer = new FeatureLayer(layer.url);
+
+            var pHead = "<div id='" + layer.title + "' class='spanHeader'><span >" + layer.title + "</span></div>";
+            layerReport.header = pHead;
+            layerReport.records = [];
+            layerReport.title = layer.title;
+
+            var query = new Query();
+            query.returnGeometry = false;
+            query.outFields = [
+              layer.fieldNamesString
+            ];
+            query.where = "0=0";
+            queryTask.execute(query).then(function (results)
+            {
+                var featureAttributes = results.features[0].attributes;
+
+  
+                if (layer.popupInfoTemplate) {
+                    var p = "";
+                    var tHead = "<tr>";
+                    var a = "Description";
+                    //popupInfoTitle
+                    var s3 = layer.popupInfoTitle.replace(/{/ig, "${");
+                    a=esriLang.substitute(featureAttributes, s3);
+
+                    tHead = tHead + "<th>" + a + "</th>";
+                    tHead = tHead + "</tr>";
+                    layerReport.recordHeader = tHead;
+                    layerReport.recordsHTML = "";
+
+                    for (var i = 0; i < results.features.length; i++) {
+                        f = results.features[i];
+                        var featureAttributes = f.attributes;
+                        var featureRecord = "<tr>";
+
+                        s2 = layer.popupInfoTemplate.replace(/{/ig, "${");
+                        s=esriLang.substitute(featureAttributes, s2);
+
+                        featureRecord = featureRecord + "<td>" + s + "</td>";
+
+                        featureRecord = featureRecord + "</tr>";
+                        layerReport.records.push(featureRecord);
+                        layerReport.recordsHTML = layerReport.recordsHTML + featureRecord;
+
+                    }
+
+                    var t = "<table>";
+                    t = t + layerReport.recordHeader;
+                    t = t + layerReport.recordsHTML;
+                    t = t + "</table><br/><br/>"
+                    var pp = layerReport.header + t;
+                    layerReport.dom = domConstruct.toDom(pp);
+
+                }
+                else {
+                    var tHead = "<tr>";
+                    for (var a in featureAttributes) {
+                        tHead = tHead + "<th>" + a + "</th>";
+                        //tHead = tHead + "<td>" + a + "</td>";
+                    }
+                    //tHead = tHead + "</tr></thead>";
+                    tHead = tHead + "</tr>";
+                    layerReport.recordHeader = tHead;
+                    layerReport.recordsHTML = "";
+
+                    for (var i = 0; i < results.features.length; i++) {
+                        f = results.features[i];
+                        var featureAttributes = f.attributes;
+                        var featureRecord = "<tr>";
+
+                        for (var a in f.attributes) {
+                            var s = f.attributes[a];
+                            if (s != null)
+                                s = s.toString();
+
+                            featureRecord = featureRecord + "<td>" + s + "</td>";
+                        }
+
+                        featureRecord = featureRecord + "</tr>";
+                        layerReport.records.push(featureRecord);
+                        layerReport.recordsHTML = layerReport.recordsHTML + featureRecord;
+
+                    }
+
+                    var t = "<table>";
+                    t = t + layerReport.recordHeader;
+                    t = t + layerReport.recordsHTML;
+                    t = t + "</table><br/><br/>"
+                    var pp = layerReport.header + t;
+                    layerReport.dom = domConstruct.toDom(pp);
+                }
+
+                deferred.resolve(layerReport);
+
+            });
+
+            //var queryRelated = new RelationshipQuery();
+            //queryRelated.returnGeometry = false;
+            //queryRelated.outFields = [
+            //  "*"
+            //];
+            //queryRelated.objectIds = [oid];
+            //sourceFeatureLayer.queryRelatedFeatures(queryRelated, updateRelatedResults);
+
+            //function createPhotoRecord(p, url) {
+
+            //    var featureAttributes = p.attributes;
+
+            //    var photoRecord = "<tr>";
+            //    photoRecord = photoRecord + "<td>Photo #</td><td>Location:&nbsp;" + featureAttributes.PHOTOLOCATION + "</td>";
+            //    photoRecord = photoRecord + "</tr>";
+
+
+            //    for (var attributeName in featureAttributes) {
+
+            //    }
+
+            //    photoRecord = photoRecord + "<tr><td>";
+            //    photoRecord = photoRecord + "<img src='" + url + "'/>";
+            //    photoRecord = photoRecord + "<td>Description:&nbsp;" + featureAttributes.PHOTODESCRIPTION + "</td>";
+
+            //    photoRecord = photoRecord + "</tr>";
+
+            //    var row = domConstruct.toDom(photoRecord);
+            //    domConstruct.place(row, "photoTable");
+
+            //}
+            //function updateRelatedResults(results) {
+
+            //    res = results[this.oid];
+
+            //    var resultCount = res.features.length;
+            //    for (var i = 0; i < resultCount; i++) {
+
+            //        p = res.features[i];
+            //        //get attachments
+            //        var reloid = p.attributes.objectid;
+
+            //        //request:
+            //        var req = this.relatedLayerURL + "/" + reloid + "/attachments";
+
+            //        var photosRequest = esriRequest({
+            //            url: req,
+            //            content: { f: "json" },
+            //            handleAs: "json",
+            //            callbackParamName: "callback"
+            //        });
+            //        photosRequest.then(
+            //          function (response) {
+
+            //              var attachmentCount = response.attachmentInfos.length;
+            //              for (var i = 0; i < attachmentCount; i++) {
+
+            //                  var photoURL = req + "/" + response.attachmentInfos[i].id;
+
+
+            //                  console.log(photoURL);
+            //                  createPhotoRecord(p, photoURL);
+            //              }
+
+            //          }, function (error) {
+            //              console.log("Error: ", error.message);
+            //          });
+
+            //    }
+
+
+            //}
+
+            
+            return deferred.promise;
+            
+
+        }
+    
+    }
+    );
+});
+
---
+++ js/webMapUtils.js
@@ -0,0 +1,109 @@
+﻿
+define([
+  'dojo/_base/declare',
+  "dojo/Deferred",
+  "dojo/parser",
+  "dojo/dom",
+  "esri/map", 
+  "esri/urlUtils",
+  "esri/arcgis/utils",
+  "dojo/_base/array",
+  "esri/request",
+  "esri/config",
+  "dojo/request/xhr"
+], function(
+  declare,
+  Deferred,
+  parser,
+  dom,
+  Map,
+  urlUtils,
+  arcgisUtils,
+  array,
+  esriRequest,
+  esriConfig,
+  xhr
+) {
+
+
+    return declare(null,{
+     
+        readWebMap: function (id) {
+            esriConfig.defaults.io.proxyUrl = "/proxy/proxy.ashx";
+            var deferred = new Deferred();
+
+            
+
+            arcgisUtils.getItem(id).then(function (response) {
+
+                var webMap = {};
+                webMap.title = response.item.title;
+                webMap.subtitle = "";
+                webMap.thumbnail = "http://vhb.maps.arcgis.com/sharing/rest/content/items/" + response.item.id+ "/info/" + response.item.thumbnail;
+                if (response.item.snippet != null)
+                    webMap.subtitle = response.item.snippet;
+                            
+                webMap.operationalLayers = response.itemData.operationalLayers;
+                webMap.layerList = [];
+                array.forEach(response.itemData.operationalLayers, function (l, i) {
+               
+                    var ll = {};
+                    ll.title = l.title;
+                    ll.url = l.url;
+                    ll.fieldNames = [];
+                    ll.fieldAliases = [];
+                    ll.fieldNamesString = "";
+                    ll.visible = l.visibility;
+
+                    ll.hasAttachments = false;//SBTEST
+
+                    var layerInfo = esriRequest({
+                        url: ll.url + "?f=json",
+                        handleAs: "json",
+                        sync:true
+                    });
+
+                    ll.popupInfoTemplate = l.popupInfo.description;
+                    ll.popupInfoTitle = l.popupInfo.title;
+
+                    array.forEach(l.popupInfo.fieldInfos, function (f, ii) {
+
+                        var f2 = f;
+
+                        if (f.visible) {
+                            ll.fieldNames.push(f.fieldName);
+                            ll.fieldAliases.push(f.label);
+                            ll.fieldNamesString = ll.fieldNamesString + f.fieldName + ",";
+                        }
+
+                    });
+
+                    if (ll.fieldNamesString.length > 0)
+                        ll.fieldNamesString = ll.fieldNamesString.slice(0, -1);
+
+                    if (ll.visible) {
+                        webMap.layerList.push(ll);
+
+                        layerInfo.then(function (res) {
+                            var r = res;
+                            ll.hasAttachments = res.hasAttachments;
+
+                        });
+                    }
+
+                })
+
+                deferred.resolve(webMap);
+
+
+
+            });
+            
+            return deferred.promise;
+            
+
+        }
+    }
+    );
+});
+
